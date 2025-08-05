d19ea22fd914f960034874f885b329864637ac9e
alshahrear <alshahrear777@gmail.com>
2025-08-05 11:36:41 +0800

    new

--- index.js
+++ index.js
@@ -7,13 +7,8 @@ require('dotenv').config();
 const port = process.env.PORT || 5000;
 
 // middleware
-app.use(cors({
-  origin: ['https://squirrelpeace.com', 'http://localhost:5173'],
-  credentials: true
-}));
-
+app.use(cors());
 app.use(express.json());
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.3wtib.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
